c5ca875c8353c53981614df3a18114a0218ede2f
karolina langner <karolinalangner1@gmail.com>
2021-02-23 12:31:00 +0000

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'https://patrycjahampelskaportfolio.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.patrycjahampelskaportfolio.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
+  :domain => 'localhost',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
