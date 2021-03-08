39143cdae6dc13261a1c7916a357db871a65e446
karolina langner <karolinalangner1@gmail.com>
2021-03-08 03:37:00 +0000

    added testimonial model + controller + views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_161136) do
+ActiveRecord::Schema.define(version: 2021_03_08_024439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,15 @@ ActiveRecord::Schema.define(version: 2021_02_12_161136) do
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.text "content"
+    t.string "author"
+    t.string "company"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "link"
   end
 
   create_table "users", force: :cascade do |t|
